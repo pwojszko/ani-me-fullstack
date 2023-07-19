9d3eacd7be85efb82c2db89b915b3f4a1d1ef95c
PWojszko <piotr.wojszko@gmail.com>
2023-07-19 15:35:42 +0200

    git ignore + login request

--- server/server.js
+++ server/server.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
 var cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello World" });
+const posts = [
+  {
+    username: "Kyle",
+    title: "Post 1",
+  },
+  {
+    username: "Jim",
+    title: "Post 2",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.post("/login", (req, res) => {
+  // auth user
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
 });
 
 app.listen(5000, () => {
